c0e42429bc0783eb126716049fb33e922b872f50
Dilyen <davidandohacquah@gmail.com>
2020-01-22 11:46:37 +0000

    updated server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mailApi'));
- 
-
-app.use(bodyParser.json());
-
-app.use(cors());
-
+app.use(express.static(__dirname + '/dist/TCMSUI'));
 
 app.get('/*', function(req,res) {
- res.sendFile(path.join(__dirname+'/dist/mailApi/index.html'));
- });
- 
+    
+res.sendFile(path.join(__dirname+'/dist/TCMSUI/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () =>{
- console.log("Server started...");
-});
+app.listen(process.env.PORT || 8080);
